ee8db018239a740992894060b5fe353ab24ed541
munkeawtoast <munkeawtoast@duck.com>
2024-03-27 02:55:34 +0700

    song player

--- translator-front/src/app/component/song-player/song-player.component.ts
+++ translator-front/src/app/component/song-player/song-player.component.ts
@@ -1,16 +1,47 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-song-player',
   templateUrl: './song-player.component.html',
-  styleUrls: ['./song-player.component.css']
+  styleUrls: ['./song-player.component.css'],
 })
 export class SongPlayerComponent implements OnInit {
+  audioPlayer!: HTMLAudioElement;
+  songUrl: string = 'https://samplelib.com/lib/preview/mp3/sample-12s.mp3';
+  volume: number = 1;
+  isPlaying: boolean = false;
+  songCurrentTime: number = 0;
+  songDuration: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {}
-    
+  ngOnInit(): void {
+    this.audioPlayer = document.getElementById(
+      'audio-player-player'
+    )! as HTMLAudioElement;
+    this.audioPlayer.load();
+    this.songDuration = this.audioPlayer.duration;
+  }
 
+  setVolume(volumeChangeEvent: any) {
+    this.audioPlayer.volume = volumeChangeEvent.target.value;
+  }
 
+  togglePlayPause() {
+    if (this.audioPlayer.paused) {
+      this.audioPlayer.play();
+    } else {
+      this.audioPlayer.pause();
+    }
+  }
+
+  updateTrackTime() {
+    const currTime = this.audioPlayer.currentTime;
+    const duration = this.audioPlayer.duration;
+    const trackProgress = currTime / duration;
+    document.getElementById('song-progress-progress')!.style.width = `${
+      trackProgress * 100
+    }%`;
+    console.log(document.getElementById('song-progress-progress')!);
+  }
 }
--- translator-front/src/app/service/song/song.service.ts
+++ translator-front/src/app/service/song/song.service.ts
@@ -3,30 +3,30 @@ import { Injectable } from '@angular/core';
 import { SongRequest } from 'src/app/component/request/song-request';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SongService {
-  private url = 'http://localhost:8080/song'
+  private url = 'http://localhost:8080/song';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getSong() {
-    return this.http.get<any>(`${this.url}/findByName`)
+    return this.http.get<any>(`${this.url}/findByName`);
   }
 
   getAllSong() {
-    return this.http.get<any[]>(`${this.url}/findAll`)
+    return this.http.get<any[]>(`${this.url}/findAll`);
   }
 
   createSong(songRequest: SongRequest) {
-    return this.http.post<any>(`${this.url}/addSong`,songRequest)
+    return this.http.post<any>(`${this.url}/addSong`, songRequest);
   }
 
-  deleteSong(song_id:number) {
-    return this.http.delete<any>(`${this.url}/deleteSong/${song_id}`)
+  deleteSong(song_id: number) {
+    return this.http.delete<any>(`${this.url}/deleteSong/${song_id}`);
   }
 
-  search(input:string) {
-    return this.http.post<any>(`${this.url}/search`,input)
+  search(input: string) {
+    return this.http.post<any>(`${this.url}/search`, input);
   }
 }
