d45fca2b31e2a4291691853fb1cb1c9c149cf6a0
munkeawtoast <munkeawtoast@duck.com>
2024-03-26 22:05:32 +0700

    navbar looping

--- translator-front/src/app/component/nav-bar/nav-bar.component.ts
+++ translator-front/src/app/component/nav-bar/nav-bar.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/service/authen/authentication.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  guestNavItems: NavBarItem[] = [
+    {
+      icon: 'bi-house-door',
+      title: 'Home',
+      routerLink: '/login',
+    },
+  ];
+  userNavItems: NavBarItem[] = [
+    {
+      icon: 'bi-house-door',
+      title: 'Home',
+      routerLink: '/home',
+    },
+    {
+      icon: 'bi-music-note-list',
+      title: 'My Playlist',
+      routerLink: '/playlist',
+    },
+    {
+      icon: 'bi-vinyl-fill',
+      title: 'Songs',
+      routerLink: '/song',
+    },
+  ];
 
-  constructor() { }
+  navItems: NavBarItem[] = this.guestNavItems;
 
-  ngOnInit(): void {
+  constructor(authService: AuthenticationService) {
+    // authService.getTokenPayload().role
+    this.navItems = this.userNavItems;
   }
 
-
+  ngOnInit(): void {}
 }
+
+type NavBarItem = {
+  icon: string;
+  title: string;
+  routerLink: string;
+};
